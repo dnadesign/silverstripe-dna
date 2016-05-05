4937e96e794a172be80d617fe8343dd0a5bb679d
Will Rossiter <will@fullscreen.io>
2016-05-05 15:22:03 +1200

    Only add writer if Key set

--- _config.php
+++ _config.php
@@ -3,4 +3,6 @@
 // only log warnings and errors to Raygun rather than notices. Notices should
 // be logged to the web server and available on demand rather than in error
 // reports.
-SS_Log::add_writer(new DnaRaygunLogWriter_Zend(), SS_Log::WARN, "<=");
+if(defined('SS_RAYGUN_APP_KEY')) {
+	SS_Log::add_writer(new DnaRaygunLogWriter_Zend(), SS_Log::WARN, "<=");
+}
